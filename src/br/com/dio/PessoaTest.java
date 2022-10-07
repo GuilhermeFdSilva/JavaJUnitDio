0bca389af3cefdf17f8c8550903fa485593d5774
Guilherme <francaguilherme27@gmail.com>
2022-10-07 11:54:23 -0300

    First Commit

---
+++ src/br/com/dio/PessoaTest.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+class PessoaTest {
+
+    @Test
+    void validarCalculoDeIdade() {
+        Pessoa pessoa = new Pessoa("Julia", LocalDate.of(2020, 1, 1));
+        Assertions.assertEquals(2, pessoa.getIdade());
+    }
+}
